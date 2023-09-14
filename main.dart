41162f6d9b49371c21676fe742164b654ba5f316
Roman Godun <romangodun@MacBook-Air-Roman.local>
2023-09-14 10:08:20 +0300

    first commit

---
+++ main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAQuE0ZkMOjVMXpxSMnubJ6Nuq8F9jUSLE",
+//   authDomain: "god-s-gift-fa.firebaseapp.com",
+//   projectId: "god-s-gift-fa",
+//   storageBucket: "god-s-gift-fa.appspot.com",
+//   messagingSenderId: "900840847869",
+//   appId: "1:900840847869:web:9b4d94a8194716e8344194",
+//   measurementId: "G-XXTML66596"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig)
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
