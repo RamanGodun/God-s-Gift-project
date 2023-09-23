70b095868423ba03643ddba05b4cac801e5e0037
Иван <ivan@MacBook-Air-Ivan.local>
2023-09-23 21:43:47 +0300

    fix: update admin provider

--- provider/admin_provider.dart
+++ provider/admin_provider.dart
@@ -1,7 +1,10 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:gods_gift/models/admin_model.dart';
 import 'package:gods_gift/models/cashback_model.dart';
 import 'package:gods_gift/server/DB_method.dart';
+import 'package:gods_gift/utils/utils.dart';
 
 class AdminProvider with ChangeNotifier {
   bool isLoading = false;
@@ -15,75 +18,68 @@ class AdminProvider with ChangeNotifier {
     return _cashbackInfo;
   }
 
-  Future<AdminModel?> getAdminDataAndPoints() async {
-    isLoading = true;
-    notifyListeners();
+  Future<AdminModel?> getAdminDataAndPoints(BuildContext context) async {
     try {
       final adminData =
           await DBmethod().fetchAdminDataFromFirestore('admin_data');
       if (adminData != null) {
         _adminModel = adminData;
       }
-      isLoading = false;
-      notifyListeners();
       return adminData;
     } catch (e) {
-      isLoading = false;
-      notifyListeners();
+      showSnackBar(context, 'Помилка при отримані даних!');
+
       return null;
     }
   }
 
-  Future<void> saveAdminDataAndPoints(AdminModel adminInfo) async {
-    isLoading = true;
-    notifyListeners();
+  Future<void> saveAdminDataAndPoints(
+      BuildContext context, AdminModel adminInfo) async {
     try {
       await DBmethod().saveAdminInfoOnFirebase(
         adminInfo: adminInfo,
         docName: 'admin_data',
       );
       _adminModel = adminInfo;
-      isLoading = false;
-      notifyListeners();
     } catch (e) {
-      isLoading = false;
-      notifyListeners();
+      showSnackBar(context, 'Помилка при збережені даних!');
     }
   }
 
-  Future<void> saveAdminCashback(CashbackModel cashbackInfo) async {
-    isLoading = true;
-    notifyListeners();
+  Future<void> saveAdminCashback(
+      BuildContext context, CashbackModel cashbackInfo) async {
     try {
       await DBmethod().saveAdminInfoOnFirebase(
         cashbackInfo: cashbackInfo,
         docName: 'cashback_data',
       );
       _cashbackInfo = cashbackInfo;
-      isLoading = false;
-      notifyListeners();
     } catch (e) {
-      isLoading = false;
-      notifyListeners();
+      showSnackBar(context, 'Помилка при збережені даних!');
     }
   }
 
-  Future<CashbackModel?> getAdminCashback() async {
-    isLoading = true;
-    notifyListeners();
+  Future<CashbackModel?> getAdminCashback(BuildContext context) async {
     try {
       final cashbackData =
           await DBmethod().fetchAdminDataFromFirestore('cashback_data');
       if (cashbackData != null) {
         _cashbackInfo = cashbackData;
       }
-      isLoading = false;
-      notifyListeners();
       return cashbackData;
     } catch (e) {
-      isLoading = false;
-      notifyListeners();
+      showSnackBar(context, 'Помилка при отримані даних!');
       return null;
     }
+  }
+
+  int findCashbackPercentageAmount(int inputAmount) {
+    for (int i = _cashbackInfo.spentMoneyLevel.length - 1; i >= 0; i--) {
+      if (inputAmount >= _cashbackInfo.spentMoneyLevel[i]) {
+        int foundedDiscountAmount = _cashbackInfo.discountPercentage[i];
+        return foundedDiscountAmount;
+      }
+    }
+    return 0;
   }
 }
